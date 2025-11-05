acb548e7f379ecb95450c991fc77702c59f2af75
Akansha Tripathi <109758759+Akansha-Tripathi05@users.noreply.github.com>
2025-11-05 16:00:03 +0000

    Corrected CI/CD 2

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,6 +1,10 @@
+import sys
+import os
+
+# Add project root to Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from app import create_app
-
-
 def test_home_status():
     app = create_app()
     client = app.test_client()
